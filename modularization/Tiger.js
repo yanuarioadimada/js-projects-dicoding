a02fbb1bba316f6e76e568c928be6a9080c3a7ff
yanuarioadimada <yanuarioadimada@gmail.com>
2024-11-05 00:35:51 +0700

    tes 123

---
+++ modularization/Tiger.js
@@ -0,0 +1,12 @@
+class Tiger {
+    constructor() {
+      this.strength = Math.floor(Math.random() * 100);
+    }
+   
+    growl() {
+      console.log('grrrrr!');
+    }
+  }
+   
+module.exports = Tiger;
+  // TODO 1: 
